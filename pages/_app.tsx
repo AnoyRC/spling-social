03638f9cfebd54f8003ccbfba4d44c8481ea0249
AnoyRC <38689344+AnoyRC@users.noreply.github.com>
2023-02-19 20:55:03 +0530

    InitialCommit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,32 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { WalletProvider, ConnectionProvider } from '@solana/wallet-adapter-react'
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui'
+import { clusterApiUrl } from '@solana/web3.js'
+import { useMemo } from 'react'
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets'
+
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  const network = "mainnet-beta";
+
+  const endpoint = useMemo(() => clusterApiUrl(network), [network])
+
+  const wallets = useMemo(() => [
+    new PhantomWalletAdapter(),
+  ], [network])
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <Component {...pageProps} />
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  )
+
+
 }
