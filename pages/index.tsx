47156aa5dde3efee17953fbcb6f95357e6ffdee6
AnoyRC <38689344+AnoyRC@users.noreply.github.com>
2023-02-26 22:01:12 +0530

    Feed Page UI 50% Completion

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,7 +18,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 const options = {
   rpcUrl:
-    "https://rpc.helius.xyz/?api-key=07172e08-2375-4358-9ad1-522bd8871a8e",
+    "https://rpc.helius.xyz/?api-key=002d43b6-4f76-47c2-b4e1-9c87c9a6e3d2",
   useIndexer: true,
 } as ProtocolOptions;
 
@@ -27,7 +27,6 @@ const WalletMultiButtonDynamic = dynamic(
     (await import("@solana/wallet-adapter-react-ui")).WalletMultiButton,
   { ssr: false }
 );
-
 
 
 const Home = () => {
@@ -66,7 +65,7 @@ const Home = () => {
 
     const postInitialize = async () => {
       if(socialProtocol !== null && socialProtocol !== undefined){
-        const posts = await socialProtocol.getAllPosts(15);
+        const posts = await socialProtocol.getAllPosts(33);
         setPosts(posts);
         console.log(posts);
       }
@@ -79,7 +78,60 @@ const Home = () => {
 
   return (
     <>
-    <WalletMultiButtonDynamic />
+      <div className='bg-[#F8FFE9] w-screen h-screen'>
+        <div className='bg-[#FFFFFF] border-[#166F00] border-b-[1px] w-screen h-16 fixed'>
+          <div className='flex h-full justify-center'>
+            <div className='w-1/3'></div>
+            <div className='hover:border-[#166F00] focus-within:border-[#166F00] border-[1px] rounded-full flex bg-[#EAEAEA] self-center h-[65%] w-1/3'>
+              <Image src="/SearchBtn.svg" alt="SearchButton" width={20} height={20} className="ml-4"></Image>
+              <input type="text" placeholder="Search for people or tags" className="bg-[#EAEAEA] w-full h-full rounded-full text-[#8C8C8C] mx-2 focus:outline-none"></input>
+              </div>
+              <div className='flex w-1/3 justify-center'>
+              <button className='transition ease-in delay-100 bg-[#166F00] rounded-full h-[65%] w-24 self-center flex items-center mx-1 hover:bg-[#5f8e53]'>
+                <Image src="/PenIcon.svg" alt="SearchButton" width={15} height={15} className="ml-5"></Image>
+                <h1 className='text-m ml-1'>Write</h1>
+              </button>
+              <div className='hover:bg-[#F8FFE9] bg-[#FFFFFF] rounded-full h-[65%] w-10 self-center flex items-center justify-center ml-1'>
+                <Image src="/DarkModeIcon.svg" alt="SearchButton" width={25} height={25} className=""></Image>
+              </div>
+              <div className='hover:bg-[#F8FFE9] bg-[#FFFFFF] rounded-full h-[65%] w-10 self-center flex items-center justify-center ml-1 mr-[10%]'>
+                <Image src="/AccountIcon.svg" alt="SearchButton" width={25} height={25} className=""></Image>
+              </div>
+              </div>
+            </div>
+        </div>
+        <div className='bg-[#F8FFE9] h-fit w-screen flex justify-center'>
+          <div className='w-1/3 flex justify-end mr-28'>
+            <div className='bg-[#FFFFFF] w-[50%] h-max mt-[10vh] border-[#166F00] border-[1px] rounded-[26px] flex flex-col justify-center'>
+              <div className='flex w-[100%] mb-2 mt-8'>
+                <div className='flex justify-start w-[100%]'>
+                  <Image src="/FeedActiveIcon.svg" alt="SearchButton" width={30} height={30} className="ml-4"></Image>
+                  <h1 className='text-xl ml-3 text-[#166f00]'>Your Feed</h1>
+                </div>
+                <div className=' flex justify-end w-[10%]'>
+                  <div className='bg-[#166f00] w-1.5 h-8 rounded-tl-md rounded-bl-md'></div>
+                </div>
+              </div>
+              <div className='flex w-[100%] py-2 hover:bg-[#EAEAEA]'>
+                <div className='flex justify-start w-[100%]'>
+                  <Image src="/ExploreIcon.svg" alt="SearchButton" width={30} height={30} className="ml-4"></Image>
+                  <h1 className='text-xl ml-3 text-[#000000]'>Explore</h1>
+                </div>
+              </div>
+              <div className='flex w-[100%] py-2 mb-6 hover:bg-[#EAEAEA]'>
+                <div className='flex justify-start w-[100%]'>
+                  <Image src="/ProfileIcon.svg" alt="SearchButton" width={30} height={30} className="ml-4"></Image>
+                  <h1 className='text-xl ml-3 text-[#000000]'>My Profile</h1>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='w-1/3'>
+          </div>
+          <div className='w-1/3'>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
