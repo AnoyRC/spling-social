54e62058e5d5b2b036349c42350ae2f4355b41e4
AnoyRC <38689344+AnoyRC@users.noreply.github.com>
2023-02-28 01:17:11 +0530

    feed section completion

---
+++ components/post.tsx
@@ -0,0 +1,88 @@
+import { NextPage } from "next";
+import { Post, SocialProtocol, Reply, User } from "@spling/social-protocol";
+import { WalletContextState } from "@solana/wallet-adapter-react";
+import { useEffect, useState } from "react";
+import Image from "next/image";
+
+interface Props {
+  post: Post | undefined;
+  socialProtocol: SocialProtocol | undefined;
+  walletAddress: WalletContextState | undefined;
+  user: User | null | undefined;
+}
+
+const Posts: NextPage<Props> = (props: Props) => {
+  const [date, setDate] = useState<String>();
+  const [tags, setTags] = useState<String[]>([]);
+
+  useEffect(() => {
+    if (props.post?.timestamp) {
+      var date = new Date(props.post?.timestamp * 1000); //Scaring me
+      setDate(
+        date.getDate() +
+          " " +
+          date.toString().substring(4, 7) +
+          " " +
+          date.getFullYear()
+      ); 
+    }
+    var tags = props.post?.tags[0].split(",");
+    if(tags)  setTags(tags);
+  }, [props.post?.timestamp]);
+
+  return (
+    <div className="bg-[#FFFFFF] border-[#166f00] border-b-[1px] w-[100%] h-fit">
+      <div className="ml-7 mt-4 flex">
+        {props.post?.user.avatar && (
+          <Image
+            src={props.post?.user.avatar}
+            alt="avatar"
+            width={58}
+            height={58}
+            className="rounded-full h-[50px] w-[50px]"
+          ></Image>
+        )}
+        <div className="flex flex-col ml-3">
+          <h1 className="text-[#505050] text-xl">
+            {props.post?.user.nickname}
+          </h1>
+          <h1 className="text-[#5E5E5E] text-sm">{date?.toString()}</h1>
+        </div>
+      </div>
+      <div>
+        <h1 className="text-[#000000] font-medium text-2xl my-3 mx-7">{props.post?.title}</h1>
+        <div className="flex h-fit width-[100%] mb-4">
+          <h1 className="text-[#000000] text-base mx-7 w-[60%]">{props.post?.text}</h1>
+          <div className="bg-[#5E5E5E] w-[40%] h-[140px] mr-7 rounded-xl">
+          {props?.post?.media[0].file && <Image src={props?.post?.media[0].file} alt="media" width={251} height={142} className='w-[100%] h-[100%] rounded-lg'></Image>}
+          </div>
+        </div>
+      </div>
+      <div className="flex mb-5">
+        <div className="flex justify-start w-[70%] ml-5">
+          <div className="w-[fit] h-[fit] border-[#166f00] border-[1px] px-1 rounded-md ml-2">
+            <h1 className="text-[#000000]">{tags[0]}</h1>
+          </div>
+          <div className="w-[fit] h-[fit] border-[#166f00] border-[1px] px-1 rounded-md ml-2">
+            <h1 className="text-[#000000]">{tags[1]}</h1>
+          </div>
+          {tags.length > 2 && (
+            <div className="w-[fit] h-[fit] border-[#166f00] border-[1px] px-1 rounded-md ml-2">
+              <h1 className="text-[#000000]">{`+${tags.length-2}`}</h1>
+            </div>
+          )}
+        </div>
+        <div className="flex justify-end w-[100%] items-center mr-7">
+          <div className='hover:bg-[#F8FFE9] hover:border-[#166F00] hover:border-[1px] bg-[#FFFFFF] rounded-full px-3 h-7 w-12 self-center flex items-center justify-center ml-1'>
+              <Image src="/LikeIcon.svg" alt="SearchButton" width={15} height={15} className=""></Image>
+              <h1 className="text-[#000000] text-lg ml-1">{props.post?.likes.length}</h1>
+          </div>
+          <div className='hover:bg-[#F8FFE9] hover:border-[#166F00] hover:border-[1px] bg-[#FFFFFF] rounded-full px-1.5 h-7 w-8 self-center flex items-center justify-center ml-1'>
+              <Image src="/CommentIcon.svg" alt="SearchButton" width={15} height={15} className=""></Image>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Posts;
--- pages/index.tsx
+++ pages/index.tsx
@@ -13,6 +13,7 @@ import {
 import dynamic from "next/dynamic";
 import { useEffect, useState } from 'react';
 import { Keypair } from '@solana/web3.js';
+import Posts from '@/components/post';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -35,6 +36,7 @@ const Home = () => {
   const [userInfo, setUserInfo] = useState<User | null>();
   const [status,setStatus] = useState<boolean>(false);
   const [posts, setPosts] = useState<Post[]>();
+  const [isFeatured, setIsFeatured] = useState<boolean>(true);
 
   const solanaWallet = useWallet();
 
@@ -91,18 +93,18 @@ const Home = () => {
                 <Image src="/PenIcon.svg" alt="SearchButton" width={15} height={15} className="ml-5"></Image>
                 <h1 className='text-m ml-1'>Write</h1>
               </button>
-              <div className='hover:bg-[#F8FFE9] bg-[#FFFFFF] rounded-full h-[65%] w-10 self-center flex items-center justify-center ml-1'>
+              <div className='hover:bg-[#F8FFE9] hover:border-[#166F00] hover:border-[1px] bg-[#FFFFFF] rounded-full h-[65%] w-10 self-center flex items-center justify-center ml-1'>
                 <Image src="/DarkModeIcon.svg" alt="SearchButton" width={25} height={25} className=""></Image>
               </div>
-              <div className='hover:bg-[#F8FFE9] bg-[#FFFFFF] rounded-full h-[65%] w-10 self-center flex items-center justify-center ml-1 mr-[10%]'>
+              <div className='hover:bg-[#F8FFE9] hover:border-[#166F00] hover:border-[1px] bg-[#FFFFFF] rounded-full h-[65%] w-10 self-center flex items-center justify-center ml-1 mr-[10%]'>
                 <Image src="/AccountIcon.svg" alt="SearchButton" width={25} height={25} className=""></Image>
               </div>
               </div>
             </div>
         </div>
-        <div className='bg-[#F8FFE9] h-fit w-screen flex justify-center'>
-          <div className='w-1/3 flex justify-end mr-28'>
-            <div className='bg-[#FFFFFF] w-[50%] h-max mt-[10vh] border-[#166F00] border-[1px] rounded-[26px] flex flex-col justify-center'>
+        <div className='bg-[#F8FFE9] h-max w-screen flex justify-center'>
+          <div className='w-1/3 flex justify-end'>
+            <div className='bg-[#FFFFFF] w-[17%] h-max mt-[96px] border-[#166F00] border-[1px] rounded-[26px] flex flex-col justify-center mr-10 fixed'>
               <div className='flex w-[100%] mb-2 mt-8'>
                 <div className='flex justify-start w-[100%]'>
                   <Image src="/FeedActiveIcon.svg" alt="SearchButton" width={30} height={30} className="ml-4"></Image>
@@ -127,6 +129,37 @@ const Home = () => {
             </div>
           </div>
           <div className='w-1/3'>
+            <div className='bg-[#FFFFFF] border-[#166f00] border-[1px] rounded-[26px] w-[100%] h-fit pb-8 mt-[96px] flex flex-col'>
+              <div className='bg-[#FFFFFF] w-[100%] h-[50px] rounded-t-[26px] border-[#166f00] border-b-[1px] flex'>
+               {isFeatured && (<><div className='flex flex-col justify-center'>
+                  <div className='flex items-center ml-7 h-[100%]'>
+                    <Image src="/FeaturedActiveIcon.svg" alt="Featured" width={16} height={16} ></Image>
+                    <h1 className='text-[#166f00] ml-2 text-lg'>Featured</h1>
+                  </div>
+                  <div className='bg-[#166f00] justify-end flex flex-col w-[80%] h-[4px] self-center rounded-t-md ml-7'></div>
+                </div>
+                <div className='flex items-center ml-5 pb-1 px-2 hover:bg-[#EAEAEA]' onClick={()=>{setIsFeatured(false)}}>
+                  <Image src="/PersonalizedIcon.svg" alt="Personalized" width={13} height={13} ></Image>
+                  <h1 className='text-[#000000] ml-2 text-lg'>Personalized</h1>
+                </div></>) || 
+                <>
+                <div className='flex items-center ml-5 pb-1 px-2 hover:bg-[#EAEAEA]' onClick={()=>{setIsFeatured(true)}}>
+                  <Image src="/FeaturedIcon.svg" alt="Featured" width={16} height={16} ></Image>
+                  <h1 className='text-[#000000] ml-2 text-lg'>Featured</h1>
+                </div>
+                <div className='flex flex-col justify-center'>
+                <div className='flex items-center ml-5 h-[100%]'>
+                  <Image src="/PersonalizedActiveIcon.svg" alt="Personalized" width={13} height={13} className='mb-[0.5px]' ></Image>
+                  <h1 className='text-[#166f00] ml-2 text-lg'>Personalized</h1>
+                </div>
+                <div className='bg-[#166f00] justify-end flex flex-col w-[85%] h-[4px] self-center rounded-t-md ml-7'></div>
+              </div></>}
+              </div>
+              <>
+              {posts && posts.map((post,index) => (
+                <Posts key={index} post={post} socialProtocol={socialProtocol} user = {userInfo} walletAddress = {walletAddress} />))}
+              </>
+            </div>
           </div>
           <div className='w-1/3'>
           </div>
