c8dbdc84884495423186b3805c97e407e0255fc6
ragingrahul <raj17sarma@gmail.com>
2023-03-04 01:27:41 +0530

    Post Function

--- components/tags.tsx
+++ components/tags.tsx
@@ -1,8 +1,14 @@
 import { useEffect, useState } from "react"
+import { NextPage } from "next";
 
-function TagsInput() {
+interface Props{
+    tags:string[];
+    setTags:(tags:string[])=>void
+}
 
-    const [tags, setTags] = useState<string[]>(["HTML","CSS"])
+const TagsInput: NextPage<Props> = (props: Props) => {
+
+    //const [tags, setTags] = useState<string[]>([])
     const handleKeyDown=(evt:React.KeyboardEvent<HTMLInputElement>)=>{
 
         if(evt.key!=='Enter')return
@@ -12,18 +18,18 @@ function TagsInput() {
         
         (evt.target as HTMLInputElement).value=""
 
-        setTags([...tags,value])
+        props?.setTags([...props?.tags,value])
 
         
     }
     const removeTag=(index:number)=>{
-        setTags(tags.filter((el,i)=>i!==index))
+        props?.setTags(props?.tags.filter((el,i)=>i!==index))
     }
 
     return (
         <div className="flex flex-row">
             <>
-                {tags.map((tag, index) => {
+                {props?.tags.map((tag, index) => {
                     return(<div className="bg-gray-300 flex flex-row rounded-full px-2 h-[25px]" key={index}>
                         <h1 className="">{tag}</h1>
                         <div className="bg-gray-600 rounded-full ml-1 h-[20px] w-[20px] mt-1 text-white text-center flex justify-center items-center hover:cursor-pointer" onClick={()=>{removeTag(index)}}>
@@ -32,7 +38,7 @@ function TagsInput() {
                     </div>)
                 })}
             </>
-            <input type="text" onKeyDown={handleKeyDown} className="tags-input" placeholder="Type somthing" />
+            <input type="text" onKeyDown={handleKeyDown} className="p-[8px]" placeholder="Type something" />
         </div>
     )
 }
